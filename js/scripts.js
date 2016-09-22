c96d9134f6829190062ca181c9f34ea27217f816
Andrew Erickson and Michael Munyaneza <michael@>
2016-09-21 19:38:04 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  $("form#interests").submit(function(event) {
+    var sport = $("input:radio[name=sport]:checked").val();
+
+    if (sport === 'Golf'){
+      $("#Golf_text").show();
+    }
+    else if (sport === 'Basketball'){
+      $("#Basketball_text").show();
+    }
+    else if (sport === 'Baseball'){
+      $("#Baseball_text").show();
+    }
+    else
+      $("#Sorry_text").show();
+    // var weather = $("input:radio[name=weather]:checked").val();
+    // var type = $("input:radio[name=type]:checked").val();
+    // var transportation = $("input:radio[name=transportation]:checked").val();
+    // var price = $("input:radio[name=price]:checked").val();
+    //
+    // if (sport === 'Africa' && "weather" === 'Dry' && "price" === 'Affordable' && "type" === 'Nature' && "transportation" === 'Boat' || 'Plane'){
+    //   $("#Africa_text").show();
+    // }
+    // else if (sport === 'Europe' && "weather" === 'Humid' && "type" === 'History' && "price" === 'Expensive' && "transportation" === 'Boat' || 'Plane'){
+    //   $("#Europe_text").show();
+    // }
+    // else if (sport === 'America' && "weather" === 'Dry' || 'Humid' && "type" === 'Fun' && "price" === 'Expensive' && "transportation" === 'Plane' || 'Car'){
+    //   $("#America_text").show();
+  event.preventDefault();
+});
+});
